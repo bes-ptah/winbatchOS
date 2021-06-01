1933ffba7d980a7abf7b650045f07b135173b3a0
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2021-06-01 14:35:44 -0600

    Update update.bat

--- update/update.bat
+++ update/update.bat
@@ -6,11 +6,11 @@ rem 0x3 Unknown Error.
 
 
 IF %_build%==1558 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b
-IF %_build%==1560 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
-IF %_build%==1562 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
-IF %_build%==1564 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
-IF %_build%==1566 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
-IF %_build%==1568 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
+IF %_build%==1560 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b
+IF %_build%==1562 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b
+IF %_build%==1564 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b
+IF %_build%==1566 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b
+IF %_build%==1568 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b
 IF %_build%==1570 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
 IF %_build%==1572 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
 IF %_build%==1574 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
@@ -20,6 +20,7 @@ IF %_build%==1580 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /
 IF %_build%==1582 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
 IF %_build%==1584 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
 IF %_build%==1586 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
+IF %_build%==1588 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
 
 SET update=0x3
 exit /b
