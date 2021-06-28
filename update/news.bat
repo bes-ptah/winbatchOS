c257fd5e62853e9bfad3567ff49f804a5e70d215
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2021-06-28 08:59:05 -0500

    Update news.bat

--- update/news.bat
+++ update/news.bat
@@ -5,4 +5,8 @@ set "Newest-Version-Release=Announcing WinBatchX 16.0"
 set "Newest-Build-Release=Announcing WinBatchX Build 1615"
 set "Special-News=0"
 
+set "Newest-Version-Release-Link="
+set "Newest-Build-Release-Link="
+set "Special-News-Link="
+
 exit /b
