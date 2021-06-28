071ec0a4fdb0a6c3f6ca88c690b873f7a60560c9
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2021-06-28 08:51:31 -0500

    Create news.bat

---
+++ update/news.bat
@@ -0,0 +1,8 @@
+:: News about WinBatchX
+@ECHO OFF
+:: For WinBatchX Search Bar and WinBatchX Widgets.
+set "Newest-Version-Release=Announcing WinBatchX 16.0"
+set "Newest-Build-Release=Announcing WinBatchX Build 1615"
+set "Special-News=0"
+
+exit /b
