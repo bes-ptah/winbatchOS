ddce258aff4831cfb67c43020ed762b783133857
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2021-07-09 16:10:54 -0700

    Update news.bat

--- update/news.bat
+++ update/news.bat
@@ -2,11 +2,11 @@
 @ECHO OFF
 :: For WinBatchX Search Bar and WinBatchX Widgets.
 set "Newest-Version-Release=Announcing WinBatchX 16.0"
-set "Newest-Build-Release=Announcing WinBatchX Build 1615"
+set "Newest-Build-Release=Announcing WinBatchX Build 1617"
 set "Special-News=0"
 
 set "Newest-Version-Release-Link=https://github.com/bes-ptah/WinBatchX/releases/tag/1600-16.0"
-set "Newest-Build-Release-Link=https://github.com/bes-ptah/WinBatchX/releases/tag/1614"
+set "Newest-Build-Release-Link=https://github.com/bes-ptah/WinBatchX/releases/tag/1617"
 set "Special-News-Link="
 
 exit /b
