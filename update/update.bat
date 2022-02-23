de6f0564c04df507e47cb99c1f51852aeab01a81
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2022-02-23 17:18:45 -0600

    Update update.bat

--- update/update.bat
+++ update/update.bat
@@ -22,14 +22,19 @@ IF %_build%==1574 set update.exe=0x02 &set update-status-action.exe=0x02 &del ne
 IF %_build%==1576 set update.exe=0x02 &set update-status-action.exe=0x02 &del news.bat &del readme.md &exit /b
 IF %_build%==1578 set update.exe=0x02 &set update-status-action.exe=0x02 &del news.bat &del readme.md &exit /b
 
-rem 16.0 Release Notes
-
 rem 16.0 notes
 rem 16.0 release - Check the EoR (End of Release) in wiki page.
 IF %_build%==1600 set update.exe=0x01 &set update-status-action.exe=0x00 &del news.bat &del readme.md &exit /b
 
-rem 16.1 notes
-IF %_build%==1600-1000 set update.exe=0x00 &set update-status-action.exe=0x00 &del news.bat &del readme.md  &del ./SECURITY.md &exit /b
+rem 16.1 release beta builds:
+IF %_build%==1600.1000 set update.exe=0x00 &set update-status-action.exe=0x00 &del news.bat &del readme.md  &del ./SECURITY.md &exit /b
+IF %_build%==1600.1500 set update.exe=0x00 &set update-status-action.exe=0x00 &del news.bat &del readme.md  &del ./SECURITY.md &exit /b
+IF %_build%==1600.2525 set update.exe=0x00 &set update-status-action.exe=0x00 &del news.bat &del readme.md  &del ./SECURITY.md &exit /b
+IF %_build%==1600.6000 set update.exe=0x00 &set update-status-action.exe=0x00 &del news.bat &del readme.md  &del ./SECURITY.md &exit /b
+IF %_build%==1600.7575 set update.exe=0x00 &set update-status-action.exe=0x00 &del news.bat &del readme.md  &del ./SECURITY.md &exit /b
+
+rem Official 16.1 build (initial too)
+IF %_build%==1600.10000 set update.exe=0x00 &set update-status-action.exe=0x00 &del news.bat &del readme.md  &del ./SECURITY.md &exit /b
 
 IF %_build%==1601 set update.exe=0x01 &set update-status-action.exe=0x01 &del news.bat &del readme.md &exit /b
 IF %_build%==1602 set update.exe=0x01 &set update-status-action.exe=0x01 &del news.bat &del readme.md &exit /b
