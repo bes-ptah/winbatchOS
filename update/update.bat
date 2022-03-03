ac8da726b95c7e2bd0f16512c77e21bd93086a85
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2022-03-02 18:48:26 -0600

    simplified build update numbers so it's quick, not slow

--- update/update.bat
+++ update/update.bat
@@ -6,25 +6,20 @@ rem 0x01 Not latest release.
 rem 0x02 Unsupported.
 rem 0x03 Unknown Error.
 
-
-IF %_build%==1558 set update.exe=0x02 &set update-status-action.exe=0x02 &del news.bat &del readme.md &exit /b
-IF %_build%==1560 set update.exe=0x02 &set update-status-action.exe=0x02 &del news.bat &del readme.md &exit /b
-IF %_build%==1562 set update.exe=0x02 &set update-status-action.exe=0x02 &del news.bat &del readme.md &exit /b
-IF %_build%==1564 set update.exe=0x02 &set update-status-action.exe=0x02 &del news.bat &del readme.md &exit /b
-IF %_build%==1566 set update.exe=0x02 &set update-status-action.exe=0x02 &del news.bat &del readme.md &exit /b
-IF %_build%==1568 set update.exe=0x02 &set update-status-action.exe=0x02 &del news.bat &del readme.md &exit /b
-IF %_build%==1570 set update.exe=0x02 &set update-status-action.exe=0x02 &del news.bat &del readme.md &exit /b
+rem Build 1558+ are 16.0 beta/pre-release builds.
+IF %_build% GTR 1558 IF %_build% LSS 1570 set update.exe=0x02 &set update-status-action.exe=0x02 &del news.bat &del readme.md &exit /b
 
 rem 15.5 Update - Check the EoR (End of Release) in wiki page.
 IF %_build%==1572 set update.exe=0x01 &set update-status-action.exe=0x01 &del news.bat &del readme.md &exit /b
 
+:: Build 1574+ "release testing" stage for 16.0 releases
 IF %_build%==1574 set update.exe=0x02 &set update-status-action.exe=0x02 &del news.bat &del readme.md &exit /b
 IF %_build%==1576 set update.exe=0x02 &set update-status-action.exe=0x02 &del news.bat &del readme.md &exit /b
 IF %_build%==1578 set update.exe=0x02 &set update-status-action.exe=0x02 &del news.bat &del readme.md &exit /b
 
-rem 16.0 notes
 rem 16.0 release - Check the EoR (End of Release) in wiki page.
 IF %_build%==1600 set update.exe=0x01 &set update-status-action.exe=0x00 &del news.bat &del readme.md &exit /b
+
 
 rem 16.1 release beta builds:
 IF %_build%==1600.1000 set update.exe=0x00 &set update-status-action.exe=0x00 &del news.bat &del readme.md &exit /b
@@ -33,52 +28,31 @@ IF %_build%==1600.2525 set update.exe=0x00 &set update-status-action.exe=0x00 &d
 IF %_build%==1600.6000 set update.exe=0x00 &set update-status-action.exe=0x00 &del news.bat &del readme.md &exit /b
 IF %_build%==1600.7575 set update.exe=0x00 &set update-status-action.exe=0x00 &del news.bat &del readme.md &exit /b
 
-rem Official 16.1 build (initial too)
+
+rem Build 1600.10000 is the initial 16.1 release (and only)
 IF %_build%==1600.10000 set update.exe=0x00 &set update-status-action.exe=0x00 &del news.bat &del readme.md &exit /b
 
-IF %_build%==1601 set update.exe=0x01 &set update-status-action.exe=0x01 &del news.bat &del readme.md &exit /b
-IF %_build%==1602 set update.exe=0x01 &set update-status-action.exe=0x01 &del news.bat &del readme.md &exit /b
-IF %_build%==1603 set update.exe=0x01 &set update-status-action.exe=0x01 &del news.bat &del readme.md &exit /b
-IF %_build%==1604 set update.exe=0x01 &set update-status-action.exe=0x01 &del news.bat &del readme.md &exit /b
-IF %_build%==1605 set update.exe=0x01 &set update-status-action.exe=0x01 &del news.bat &del readme.md &exit /b
-IF %_build%==1606 set update.exe=0x01 &set update-status-action.exe=0x01 &del news.bat &del readme.md &exit /b
-IF %_build%==1607 set update.exe=0x01 &set update-status-action.exe=0x01 &del news.bat &del readme.md &exit /b
-IF %_build%==1608 set update.exe=0x01 &set update-status-action.exe=0x01 &del news.bat &del readme.md &exit /b
-IF %_build%==1609 set update.exe=0x01 &set update-status-action.exe=0x01 &del news.bat &del readme.md &exit /b
-IF %_build%==1610 set update.exe=0x01 &set update-status-action.exe=0x01 &del news.bat &del readme.md &exit /b
-IF %_build%==1611 set update.exe=0x01 &set update-status-action.exe=0x01 &del news.bat &del readme.md &exit /b
-IF %_build%==1612 set update.exe=0x01 &set update-status-action.exe=0x01 &del news.bat &del readme.md &exit /b
-IF %_build%==1613 set update.exe=0x01 &set update-status-action.exe=0x01 &del news.bat &del readme.md &exit /b
-IF %_build%==1614 set update.exe=0x01 &set update-status-action.exe=0x01 &del news.bat &del readme.md &exit /b
+rem Builds 1601+ are 16.0 updates/fixes + will be added to WBX 16.1
+rem Builds 1615+ are prerelease WBX-17 Builds
+IF %_build% GTR 1601 IF %_build% LSS 1632 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &exit /b
 
-rem Builds 1615 and up will show the changelog about WinBatchX. 
-rem Pre-release WBX-17 builds!
-IF %_build%==1615 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &exit /b
-IF %_build%==1616 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &exit /b
-IF %_build%==1617 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &exit /b
-IF %_build%==1618 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &exit /b
-IF %_build%==1619 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &exit /b
-IF %_build%==1620 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &exit /b
-IF %_build%==1621 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &exit /b
-IF %_build%==1622 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &exit /b
-IF %_build%==1623 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &exit /b
-IF %_build%==1624 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &exit /b
-IF %_build%==1625 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &exit /b
-IF %_build%==1626 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &exit /b
-IF %_build%==1627 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &exit /b
-IF %_build%==1628 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &exit /b
-IF %_build%==1629 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &exit /b
-IF %_build%==1630 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &exit /b
-IF %_build%==1631 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &exit /b
-IF %_build%==1632 set update.exe=0x00 &set update-status-action.exe=0x00 &call news.bat &del news.bat &del readme.md &exit /b
+rem Current build: 1633
+IF %_build%==1633 set update.exe=0x00 &set update-status-action.exe=0x00 &call news.bat &del news.bat &del readme.md &exit /b
 
-rem Pre-release (but unstable) builds of WBX-17 with WIN11 UI.
-IF %_build%==1745 set update.exe=0x00 &set update-status-action.exe=0x00 &set update-newer-release-message="You are on the latest build." &call news.bat &del news.bat &exit /b
+rem Builds 1634+ are beta WBX-17 Builds
+rem Builds 1670+ are "release testing" WBX-17 builds
+rem Build  1700 is 17.0's release
 
 
-IF %_build%==1638 set update.exe=0x00 &set update-settings.exe="You are on the latest build." &del readme.md &call news.bat &del news.bat &call patchupdates.bat &del patchupdates.bat &call updateWBX.bat &del updateWBX.bat
-:: 1650+
-:: basically all of these update ".bat" files "print" them to a variable assigned. Then when WinBatchX Settings ask for a update (when you manually press the button) it will redownload and update again. (1700+ for upgrading WBX, not finished)
+rem WBX-18 possibly? No. Just working on a GUI manager for now with a custom build! (I might even release this to beta)
+rem Also aiming build 2000 for 18.0, going off the build system (16.0 = 1600, 17.0 = 1700, 18.0 = 2000- see the difference)
+IF %_build%==1747 set update.exe=0x00 &set update-status-action.exe=0x00 &set update-newer-release-message="You are on the latest build." &call news.bat &del news.bat &exit /b
+
+
+
+
+
+
 
 SET update.exe=0x03
 SET update-status-action.exe=0x03
