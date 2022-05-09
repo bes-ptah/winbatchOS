e9c7e6fc6511d4ef6636167ab72a06f9a1470d5e
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2022-05-09 17:57:40 -0500

    Update update.bat

--- update/update.bat
+++ update/update.bat
@@ -1,6 +1,6 @@
 ECHO OFF
 
-rem Exit codes:
+rem Exit codes: (for pre15.5 - 16.0)
 rem 0x00 Supported.
 rem 0x01 Not latest release.
 rem 0x02 Unsupported.
@@ -40,17 +40,28 @@ rem Builds 1601+ are 16.0 updates/fixes + will be added to WBX 16.1
 rem Builds 1615+ are prerelease WBX-17 Builds
 IF %_build% GTR 1601 IF %_build% LSS 1632 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
 
-rem Current build: 1633
 IF %_build%==1633 set update.exe=0x00 &set update-status-action.exe=0x00 &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
 
+
+
+
+
+
+
+
 rem Builds 1634+ are beta WBX-17 Builds
+rem (!) Note the way updates are recieved are now changed.
+
+rem _WBXCore-update = (0 = up to date) (1 = not up to date) (2 = need to update, out of support) (3 = unknown error)
+rem _WBXCore-latestrelease = (message for the latest release, or build)
+rem _WBXCore-updatealert = (message for anything - major bug problems etc)
+IF %_build%==1634 set "_WBXCORE-update=0" &set "_WBXCORE-latestrelease=You're on the latest build!" &set "_update-alert=You are on the beta channel. Note- you may get unstable experiences with this build." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
+
+IF %_build%==1635 set "_WBXCORE-update=0" &set "_WBXCORE-latestrelease=Test-latestrelease" &set "_update-alert=Test-alert=test-alert" &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
+
 rem Builds 1670+ are "release testing" WBX-17 builds
 rem Build  1700 is 17.0's release
 
-
-rem WBX-18 possibly? No. Just working on a GUI manager for now with a custom build! (I might even release this to beta)
-rem Also aiming build 2000 for 18.0, going off the build number system (16.0 = 1600, 17.0 = 1700, 18.0 = 2000- see the difference)
-IF %_build%==1747 set update.exe=0x00 &set update-status-action.exe=0x00 &set update-newer-release-message="You are on the latest build." &call news.bat &del news.bat &del upgrade.bat &exit /b
 
 
 
@@ -61,6 +72,7 @@ IF %_build%==1747 set update.exe=0x00 &set update-status-action.exe=0x00 &set up
 SET update.exe=0x03
 SET update-status-action.exe=0x03
 del news.bat
+del upgrade.bat
 del readme.md
 exit /b
 
