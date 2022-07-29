c451023fc312d50521a1fbae19794747ef9bf827
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2022-07-29 11:38:55 -0400

    Update update.bat

--- update/update.bat
+++ update/update.bat
@@ -60,7 +60,7 @@ IF %_build%==1635 set "_WBXCore-update=1" &set "_WBXCore-updatemessage=A newer b
 IF %_build%==1636 set "_WBXCore-update=0" &set "_WBXCore-updatemessage=You're up to date!" &set "_WBXCore-updatealert=You are on the beta channel. Note- you may get unstable experiences with this build." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
 
 rem devbuild in check work
-IF %_build%==1637 set "_WBXCore-update=0" &set "_WBXCore-updatemessage=You're up to date!" &set "_WBXCore-updatealert=You are on the beta channel. Note- you may get unstable experiences with this build." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
+IF %_build%==1638 set "_WBXCore-update=0" &set "_WBXCore-updatemessage=You're up to date!" &set "_WBXCore-updatealert=You are on the beta channel. Note- you may get unstable experiences with this build." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
 
 rem Builds 1670+ are "release testing" WBX-17 builds
 rem Build  1700 is 17.0's release
