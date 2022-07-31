950b5d853ee8c04ef89794832b2b8e2bd871fd95
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2022-07-30 21:31:06 -0400

    Update update.bat

--- update/update.bat
+++ update/update.bat
@@ -57,10 +57,12 @@ rem _WBXCore-updatemessage = (message for the latest release, or build)
 rem _WBXCore-updatealert = (message for anything - major bug problems etc)
 IF %_build%==1634 set "_WBXCore-update=1" &set "_WBXCore-updatemessage=A newer build is in github" &set "_WBXCore-updatealert=This is not the latest build. The latest build is 1636." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
 IF %_build%==1635 set "_WBXCore-update=1" &set "_WBXCore-updatemessage=A newer build is in github" &set "_WBXCore-updatealert=This is not the latest build. The latest build is 1636." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
-IF %_build%==1636 set "_WBXCore-update=0" &set "_WBXCore-updatemessage=You're up to date!" &set "_WBXCore-updatealert=You are on the beta channel. Note- you may get unstable experiences with this build." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
+IF %_build%==1636 set "_WBXCore-update=1" &set "_WBXCore-updatemessage=A newer build is in github" &set "_WBXCore-updatealert=This is not the latest build. The latest build is 1636." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
+rem 1637 is not a build
 
-rem devbuild in check work
+rem 17.0 Developer Builds, major changes
 IF %_build%==1638 set "_WBXCore-update=0" &set "_WBXCore-updatemessage=You're up to date!" &set "_WBXCore-updatealert=You are on the beta channel. Note- you may get unstable experiences with this build." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
+IF %_build%==1639 set "_WBXCore-update=0" &set "_WBXCore-updatemessage=You're up to date!" &set "_WBXCore-updatealert=You are on the beta channel. Note- you may get unstable experiences with this build." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
 
 rem Builds 1670+ are "release testing" WBX-17 builds
 rem Build  1700 is 17.0's release
