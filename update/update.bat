99062d4d1293ef3461ee2580c3d6f3b3f38f9f0c
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2022-08-10 18:47:15 -0400

    Update update.bat

--- update/update.bat
+++ update/update.bat
@@ -85,7 +85,7 @@ cd ..
 cd ..
 CALL Button 40 20 f0 "WinBatchX 17.0, NI Version 11" X _Button_Boxes _Button_Hover
 CALL Button 40 13 f0 "Do you want to install a new version?" X _Button_Boxes _Button_Hover
-CALL Button 100 13 f3 "Yes" 110 13 f4 "No" X _Button_Boxes _Button_Hover
+CALL Button 100 13 f3 "Yes" 115 13 f4 "No" X _Button_Boxes _Button_Hover
 
 GetInput /M %_BOX% %_Button_Boxes%
 SET _SELECTION=%Errorlevel%
