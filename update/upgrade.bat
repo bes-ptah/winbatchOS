06c6667ebff814c5b31da624a55ec94b166c773b
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2022-08-10 18:20:39 -0400

    Update upgrade.bat

--- update/upgrade.bat
+++ update/upgrade.bat
@@ -1,3 +1,5 @@
+cd ..
+cd ..
 CALL Button 40 16 f0 "WinBatchX 17.0, NI Version 11" X _Button_Boxes _Button_Hover
 CALL Button 40 13 f0 "Do you want to install a new version?" X _Button_Boxes _Button_Hover
 CALL Button 50 13 f3 "Yes" 55 13 fr "No" X _Button_Boxes _Button_Hover
@@ -6,7 +8,7 @@ GetInput /M %_BOX% %_Button_Boxes%
 SET _SELECTION=%Errorlevel%
 
 IF %Errorlevel%==1 GOTO :UPGRADECORE
-IF %Errorlevel%==2 exit /b
+IF %Errorlevel%==2 cd winbatchx-main &cd update &exit /b
 
 
 
