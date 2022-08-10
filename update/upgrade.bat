59ec5b276eabade6822ce7c78c4a145ccc7fc952
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2022-08-10 17:58:41 -0400

    Update upgrade.bat

--- update/upgrade.bat
+++ update/upgrade.bat
@@ -1,6 +1,6 @@
-wget -q "https://github.com/bes-ptah/WinBatchX/releases/download/1638/WinBatchXBuild1638.zip" > nul
+:: wget -q "https://github.com/bes-ptah/WinBatchX/releases/download/1638/WinBatchXBuild1638.zip" > nul
 
-move WinBatchXBuild1638.zip ../
+:: move WinBatchXBuild1638.zip ../
 
 
 
