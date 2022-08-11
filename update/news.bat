c79089998969d4d893c1982bd7180195453d173e
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2022-08-10 21:37:06 -0400

    Update news.bat

--- update/news.bat
+++ update/news.bat
@@ -1,12 +1,12 @@
 :: News about WinBatchX
 @ECHO OFF
 :: For WinBatchX Search Bar and WinBatchX Widgets.
-set "Newest-Version-Release=Announcing WinBatchX 16.1"
-set "Newest-Build-Release=Announcing WinBatchX Beta Build 1636"
-set "Special-News=WinBatchX 16 support has ended! 16.1 is currently supported."
+set "Newest-Version-Release=Announcing WinBatchX 17.0"
+set "Newest-Build-Release=Announcing WinBatchX Build 1640- 17.0"
+set "Special-News=WinBatchX 17 was released! 17.0 is not a full operating system."
 
-set "Newest-Version-Release-Link=https://github.com/bes-ptah/WinBatchX/releases/tag/1600-16.0"
-set "Newest-Build-Release-Link=https://github.com/bes-ptah/WinBatchX/releases/tag/1636"
-set "Special-News-Link="
+set "Newest-Version-Release-Link=https://github.com/bes-ptah/WinBatchX/releases/tag/1640"
+set "Newest-Build-Release-Link=https://github.com/bes-ptah/WinBatchX/releases/tag/1640"
+set "Special-News-Link=https://github.com/bes-ptah/WinBatchX/"
 
 exit /b
