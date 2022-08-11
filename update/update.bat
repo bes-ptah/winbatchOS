8c427741158c2854260c2a232c58a0b9b16102dc
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2022-08-10 20:25:37 -0400

    Update update.bat

--- update/update.bat
+++ update/update.bat
@@ -27,12 +27,13 @@ rem 16.2 release - Check the EoR (End of Release) in wiki page.
 IF %_build%==1600.2525 IF %selectsetting%==4 goto :WBX-16.0 &del news.bat &del readme.md &del upgrade.bat &exit /b
 IF %_build%==1600.2525 set update.exe=0x01 &set update-status-action.exe=0x01 &del news.bat &del readme.md &del upgrade.bat &exit /b
 
+echo test
 
 rem Builds 1601+ are 16.0 updates/fixes + will be added to WBX 16.1
 rem Builds 1615+ are prerelease WBX-17 Builds
 IF %_build% GTR 1601 IF %_build% LSS 1633 set update.exe=0x01 &set update-status-action.exe=0x01 &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
 
-echo test
+
 
 
 
