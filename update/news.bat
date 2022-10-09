58389fe9fc7bbb1ed63560900cbe8013838f4fb1
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2022-10-09 16:44:43 -0500

    Update news.bat

--- update/news.bat
+++ update/news.bat
@@ -3,7 +3,7 @@
 :: For WinBatchX Search Bar and WinBatchX Widgets.
 set "Newest-Version-Release=Announcing WinBatchX 17.0"
 set "Newest-Build-Release=Announcing WinBatchX 18 Build 1662"
-set "Special-News=WinBatchX 17 was released- 17.0 is not a full operating system."
+set "Special-News=WinBatchX 17 was released. It is not a full operating system."
 
 set "Newest-Version-Release-Link=https://github.com/bes-ptah/WinBatchX/releases/tag/1640"
 set "Newest-Build-Release-Link=https://github.com/bes-ptah/WinBatchX/releases/tag/1662"
