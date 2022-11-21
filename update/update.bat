e301959cfd0e3026e0d1fa4029b6182a085807a7
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2022-11-20 20:09:18 -0500

    Test 18

--- update/update.bat
+++ update/update.bat
@@ -45,13 +45,13 @@ IF %_build%==1772 set "_WBXCore-update=1" &set "_WBXCore-updatemessage=You're up
 IF %_build%==1774 set "_WBXCore-update=1" &set "_WBXCore-updatemessage=You're up to date" &set "_WBXCore-updatealert=Working on in-place upgrades." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
 rem Preview builds
 IF %_build%==1776 set "_WBXCore-update=0" &set "_WBXCore-updatemessage=You're up to date" &set "_WBXCore-updatealert=Working on in-place upgrades." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
-IF %_build%==1778 set "_WBXCore-update=2" &set "_WBXCore-updatemessage=You're up to date" &set "_WBXCore-updatealert=Working on in-place upgrades." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
+IF %_build%==1778 set "_WBXCore-update=0" &set "_WBXCore-updatemessage=You're up to date" &set "_WBXCore-updatealert=Working on in-place upgrades." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
 
 
 
 rem 18.0 release - Check the EoR (End of Release) in wiki page.
 rem (!) just a random number, do not expect that number
-IF %_build%==1800 set "_WBXCore-update=0" &set "_WBXCore-updatemessage=You're up to date" &set "_WBXCore-updatealert=For more infomation, open widgets." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
+IF %_build%==1800 set "_WBXCore-update=1" &set "_WBXCore-updatemessage=You're up to date" &set "_WBXCore-updatealert=For more infomation, open widgets." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
 
 
 rem 18.S Preview Builds
