2d444f365d66b8b6d04532fa7dbeba2422203a6e
kaia  3 <77357313+bes-ptah@users.noreply.github.com>
2023-02-10 19:58:41 -0500

    Update update.bat

--- update/update.bat
+++ update/update.bat
@@ -59,6 +59,7 @@ rem 19.0 Release - March 2023 - Check the EoR (End of Release) in wiki page.
 
 
 IF %_sysbuild%==23.0.10000.100 set "_WBXCore-update=0" &set "_WBXCore-updatemessage=You're up to date" &set "_WBXCore-updatealert=Many changes were in this build, not everything is compitable." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
+IF %_sysbuild%==23.0.10002.100 set "_WBXCore-update=0" &set "_WBXCore-updatemessage=You're up to date" &set "_WBXCore-updatealert=Many changes were in this build, not everything is compitable." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
 
 
 
