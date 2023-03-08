9a21f2bf01fabda4044e7486d02ea490a1c28e7b
kaia  3 <77357313+bes-ptah@users.noreply.github.com>
2023-03-08 18:27:13 -0500

    Update update.bat

--- update/update.bat
+++ update/update.bat
@@ -6,7 +6,7 @@ rem _WBXCore-updatemessage = (message for the latest release, or build)
 rem _WBXCore-updatealert = (message for anything - major bug problems etc)
 
 
-IF %_build%==23.0.10008.100 set "_WBXCore-update=0" &set "_WBXCore-updatemessage=You're up to date" &set "_WBXCore-updatealert=Many changes were in this build, not everything is compitable." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
+IF %_build%==11.0.10008.100 set "_WBXCore-update=0" &set "_WBXCore-updatemessage=You're up to date" &set "_WBXCore-updatealert=Many changes were in this build, not everything is compitable." &call news.bat &del news.bat &del readme.md &del upgrade.bat &exit /b
 
 
 
