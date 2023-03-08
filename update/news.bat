f8f65e794165f319cefe3fe233af042512483647
kaia  3 <77357313+bes-ptah@users.noreply.github.com>
2023-03-08 18:33:55 -0500

    Update news.bat

--- update/news.bat
+++ update/news.bat
@@ -1,12 +1,12 @@
 :: News about WinBatchX
 @ECHO OFF
 :: For WinBatchX Search Bar and WinBatchX Widgets.
-set "Newest-Version-Release=Announcing WinBatchX 17.1 & 18"
-set "Newest-Build-Release=Announcing WinBatchX 18.1 Preview Build 1812"
+set "Newest-Version-Release=Announcing WinBatchX 17.1 & 19, Version 2302"
+set "Newest-Build-Release=Announcing WinBatchOS 11 Preview Build 10006"
 set "Special-News=WinBatchX 18 was released. Check via WinBatchX Update to get started!"
 
-set "Newest-Version-Release-Link=https://github.com/bes-ptah/WinBatchX/releases/tag/1800"
-set "Newest-Build-Release-Link=https://github.com/bes-ptah/WinBatchX/releases/tag/1778"
+set "Newest-Version-Release-Link=https://github.com/bes-ptah/WinBatchOS/releases/tag/1900.151"
+set "Newest-Build-Release-Link=https://github.com/bes-ptah/WinBatchOS/releases/tag/23.0.10006.100"
 set "Special-News-Link=Not available."
 
 exit /b
