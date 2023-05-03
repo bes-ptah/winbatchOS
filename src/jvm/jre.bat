86720f818e85c3a4d1154408ac92e3063deac8cf
kaia  3 <77357313+bes-ptah@users.noreply.github.com>
2023-05-03 11:11:15 -0500

    java setup

---
+++ src/jvm/jre.bat
@@ -0,0 +1,68 @@
+
+rem Java Runtime Environment (JVM) compitability layer batch file
+rem Version 0.16.0
+
+rem Microsoft Public License (Ms-PL)
+rem.
+rem This license governs use of the accompanying software. If you use the
+rem software, you accept this license. If you do not accept the license, do not
+rem use the software.
+rem.
+rem 1.  Definitions
+rem The terms "reproduce," "reproduction," "derivative works," and "distribution"
+rem have the same meaning here as under U.S. copyright law. A "contribution" is
+rem the original software, or any additions or changes to the software. A
+rem "contributor" is any person that distributes its contribution under this
+rem license. "Licensed patents" are a contributor's patent claims that read
+rem directly on its contribution.
+rem. 
+rem 2.  Grant of Rights
+rem      (A) Copyright Grant- Subject to the terms of this license, including the
+rem      license conditions and limitations in section 3, each contributor grants
+rem      you a non-exclusive, worldwide, royalty-free copyright license to
+rem      reproduce its contribution, prepare derivative works of its contribution,
+rem      and distribute its contribution or any derivative works that you create.
+rem.
+rem      (B) Patent Grant- Subject to the terms of this license, including the
+rem      license conditions and limitations in section 3, each contributor grants
+rem      you a non-exclusive, worldwide, royalty-free license under its licensed
+rem      patents to make, have made, use, sell, offer for sale, import, and/or
+rem      otherwise dispose of its contribution in the software or derivative works
+rem      of the contribution in the software.
+rem.
+rem 3.  Conditions and Limitations
+rem      (A) No Trademark License- This license does not grant you rights to use
+rem      any contributors' name, logo, or trademarks.
+rem.
+rem      (B) If you bring a patent claim against any contributor over patents that
+rem      you claim are infringed by the software, your patent license from such
+rem      contributor to the software ends automatically.
+rem.
+rem      (C) If you distribute any portion of the software, you must retain all
+rem      copyright, patent, trademark, and attribution notices that are present in
+rem      the software.
+rem.
+rem      (D) If you distribute any portion of the software in source code form,
+rem      you may do so only under this license by including a complete copy of
+rem      this license with your distribution. If you distribute any portion of the
+rem      software in compiled or object code form, you may only do so under a
+rem      license that complies with this license.
+rem.
+rem      (E) The software is licensed "as-is." You bear the risk of using it. The
+rem      contributors give no express warranties, guarantees, or conditions. You
+rem      may have additional consumer rights under your local laws which this
+rem      license cannot change. To the extent permitted under your local laws, the
+rem      contributors exclude the implied warranties of merchantability, fitness
+rem      for a particular purpose and non-infringement.
+
+
+@echo off
+cls
+
+
+
+rem this runs stand-alone right now with it's own files and display.
+rem Uses NI kernel.
+cd sources
+
+
